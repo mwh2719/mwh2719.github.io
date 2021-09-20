c769901593c354a4a19213f234316b065a477576
MITCHELL-COMPUT\Mitchell <mwh2719@rit.edu>
2021-09-20 13:02:11 -0400

    Updating quizmon
    added stylizing to quizmon and added more features and animations

--- projects/react/quizmon/src/App.js
+++ projects/react/quizmon/src/App.js
@@ -1,25 +1,45 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import './App.css';
 import GameScreen from './MyComponents';
 
 class App extends React.Component{
+
+    
+
+    
 
     constructor(props) {
         super(props);
 
         //setting the game state of the app to start when the app is constructed
-        this.state = { gameState: "start", results: null, questionNumber: 1, playerAnswer: null, correct: 0, wrong: 0 };
+        this.state = { gameState: "loading", results: null, questionNumber: 1, playerAnswer: null, correct: [], wrong: [], check: false, answerArray: [], answerCorrect: false };
 
         this.apiCall = this.apiCall.bind(this);
         this.increment = this.increment.bind(this);
-        this.transferToCheck = this.transferToCheck.bind(this);
+        this.checkAnswer = this.checkAnswer.bind(this);
         this.transferToEnd = this.transferToEnd.bind(this);
         this.transferToStat = this.transferToStat.bind(this);
         this.transferToStartMenu = this.transferToStartMenu.bind(this);
-        this.incrementCorrect = this.incrementCorrect.bind(this);
-        this.incrementWrong = this.incrementWrong.bind(this);
+        this.saveResult = this.saveResult.bind(this);
+        this.saveAnswerOrder = this.saveAnswerOrder.bind(this);
+        this.loaded = this.loaded.bind(this);
+
+        
     }
+
+    audioSlide = new Audio("enter.wav");
+    audioLobby = new Audio("loop.mp3");
+    
+
+    audioLoadedCount = 0;
+
+    loaded() {
+        this.audioLoadedCount++;
+        if (this.audioLoadedCount == 2) {
+            this.setState({ gameState: "start" });
+        }
+    }
+
 
     //function to retrieve the trivia questions from the JSON API
     apiCall(e) {
@@ -68,30 +88,56 @@ class App extends React.Component{
 
     }
 
+    saveAnswerOrder(array) {
+        this.setState({ answerArray: array });
+    }
+
+
     //Method that handles changing the game state to the answer check state
-    transferToCheck(e) {
-        this.setState({ gameState: "rightOrWrong", playerAnswer: e.target.value });
+    checkAnswer(e) {
+        if (e.target) {
+            if (e.target.value == this.state.results[this.state.questionNumber-1].correct_answer) {
+                this.setState({ playerAnswer: e.target.value, check: true, answerCorrect: true });
+            }
+            else {
+                this.setState({ playerAnswer: e.target.value, check: true, answerCorrect: false });
+            }
+        }
+        else {
+            this.setState({ playerAnswer: null, check: true, answerCorrect: false });
+        }
     }
 
 
     //Method that increments the question count and sets the state back to game and resets player answer
     increment() {
-        this.setState({ questionNumber: ++this.state.questionNumber, gameState: "game", playerAnswer: null });
+        this.setState({ questionNumber: ++this.state.questionNumber, playerAnswer: null, check: false, answerArray: [], answerCorrect: false });
     }
 
-    //Method to increment the number of correct responses
-    incrementCorrect() {
-        this.setState({ correct: ++this.state.correct });
-    }
-
-    //Method to increment the number of incorrect responses
-    incrementWrong() {
-        this.setState({ wrong: ++this.state.wrong });
+    //Method to save question info to array for later usage
+    saveResult(correct, questionInfo) {
+        questionInfo["questionNumber"] = this.state.questionNumber;
+        if (correct) {
+            this.setState(state => {
+                const correct = state.correct.concat(questionInfo);
+                return {
+                    correct
+                };
+            })
+        }
+        else {
+            this.setState(state => {
+                const wrong = state.wrong.concat(questionInfo);
+                return {
+                    wrong
+                };
+            })
+        }
     }
 
     //Method that changes the game state to the end sreen
     transferToEnd() {
-        this.setState({ gameState: "end", questionNumber: 1, playerAnswer: null })
+        this.setState({ gameState: "end", questionNumber: 1, playerAnswer: null})
     }
 
     //Method that changes game state to the stat screen
@@ -101,15 +147,17 @@ class App extends React.Component{
 
     //Method that changes the game state to the start menu
     transferToStartMenu() {
-        this.setState({ gameState: "start", results: null, correct: 0, wrong: 0 })
+        this.setState({ gameState: "start", results: null, correct: [], wrong: [], answerArray: [], playerAnswer: null, check: false, answerCorrect: false })
     }
+
 
     render(){
         return (
-            <div className="App">
+            <div id="app">
                 <GameScreen currentState={this.state.gameState} database={this.apiCall} results={this.state.results} questionNumber={this.state.questionNumber}
-                    incrementQuestion={this.increment} playerAnswer={this.state.playerAnswer} transferToCheck={this.transferToCheck} transferToEnd={this.transferToEnd} transferToStat={this.transferToStat}
-                    transferToStartMenu={this.transferToStartMenu} incrementWrong={this.incrementWrong} incrementCorrect={this.incrementCorrect} correctAmount={this.state.correct} wrongAmount={this.state.wrong} />
+                    incrementQuestion={this.increment} playerAnswer={this.state.playerAnswer} checkAnswer={this.checkAnswer} transferToEnd={this.transferToEnd} transferToStat={this.transferToStat}
+                    transferToStartMenu={this.transferToStartMenu} saveResult={this.saveResult} correct={this.state.correct} wrong={this.state.wrong} check={this.state.check}
+                    saveAnswerOrder={this.saveAnswerOrder} answerArray={this.state.answerArray} answerCorrect={this.state.answerCorrect} audioSlide={this.audioSlide} audioLobby={this.audioLobby} loaded={this.loaded} />
             </div>
         );
     }
