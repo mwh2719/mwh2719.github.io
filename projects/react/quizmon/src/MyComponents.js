8764c83d97142a9a427c1c88f18ea94d4c029090
MITCHELL-COMPUT\Mitchell <mwh2719@rit.edu>
2021-09-20 14:43:35 -0400

    Adding particle effects

--- projects/react/quizmon/src/MyComponents.js
+++ projects/react/quizmon/src/MyComponents.js
@@ -25,8 +25,8 @@ function Welcome(props) {
     props.audioLobby.loop = true;
 
     return (
+        <div>
         <div className="content">
-            
             <div id="welcome-div" className="welcome">
                 <CSSTransitionGroup
                     transitionName="welcome-title"
@@ -95,6 +95,8 @@ function Welcome(props) {
                     </form>
                 </div>
             </CSSTransitionGroup>
+            </div>
+            <BouncingBalls />
         </div>
     ); 
 }
@@ -146,7 +148,7 @@ function PlayGame(props) {
         ]} size={75} rotation='counterclockwise' onComplete={selectAnswer} />
     </div>;
     let results = null;
-    let particleEffect = null;
+    let particleEffect = <BouncingBalls />;
     if (props.check) {
         timer = <div id="timer-wrap"></div>;
         results = <Results correct={props.answerCorrect} nextQuestion={nextQuestion} />;
@@ -946,7 +948,129 @@ function Confetti(props) {
     )
 }
 
-
+function BouncingBalls(props) {
+    return (
+        <Particles className="particles"
+            options={{
+                fullScreen: {
+                    enable: true
+                },
+                fpsLimit: 60,
+                particles: {
+                    number: {
+                        value: 80,
+                        density: {
+                            enable: true,
+                            area: 800
+                        }
+                    },
+                    color: {
+                        value: "#ffffff"
+                    },
+                    shape: {
+                        type: "circle",
+                        stroke: {
+                            width: 0,
+                            color: "#000000"
+                        },
+                        polygon: {
+                            sides: 5
+                        },
+                        image: {
+                            src: "https://cdn.matteobruni.it/images/particles/github.svg",
+                            width: 100,
+                            height: 100
+                        }
+                    },
+                    opacity: {
+                        value: 0.5,
+                        random: false,
+                        animation: {
+                            enable: false,
+                            speed: 1,
+                            minimumValue: 0.1,
+                            sync: false
+                        }
+                    },
+                    size: {
+                        value: 10,
+                        random: { enable: true, minimumValue: 5 },
+                        animation: {
+                            enable: false,
+                            speed: 40,
+                            minimumValue: 5,
+                            sync: false
+                        }
+                    },
+                    lineLinked: {
+                        enable: false,
+                        distance: 150,
+                        color: "#ffffff",
+                        opacity: 0.4,
+                        width: 1
+                    },
+                    move: {
+                        collisions: true,
+                        enable: true,
+                        speed: 7,
+                        direction: "none",
+                        random: false,
+                        straight: false,
+                        out_mode: "bounce",
+                        attract: {
+                            enable: false,
+                            rotateX: 600,
+                            rotateY: 1200
+                        }
+                    }
+                },
+                interactivity: {
+                    detect_on: "canvas",
+                    events: {
+                        onHover: {
+                            enable: false,
+                            mode: "repulse",
+                            parallax: {
+                                enable: false,
+                                force: 60,
+                                smooth: 10
+                            }
+                        },
+                        onClick: {
+                            enable: true,
+                            mode: "push"
+                        },
+                        resize: true
+                    },
+                    modes: {
+                        grab: {
+                            distance: 400,
+                            line_linked: {
+                                opacity: 1
+                            }
+                        },
+                        bubble: {
+                            distance: 400,
+                            size: 40,
+                            duration: 2,
+                            opacity: 0.8,
+                            speed: 3
+                        },
+                        repulse: {
+                            distance: 200
+                        },
+                        push: {
+                            particles_nb: 4
+                        },
+                        remove: {
+                            particles_nb: 2
+                        }
+                    }
+                },
+                detectRetina: true,
+            }}/>
+        )
+}
 
 
 export default GameScreen
